54b93ece31cc5182906845e5d37cda37571a4bdf
GunKate <gokhfeld.ev@gmail.com>
2024-04-18 20:23:43 +0500

    begin

---
+++ task.py
@@ -0,0 +1,8 @@
+class Task():
+    def __init__ (self, discribe, date_end, status):
+        self.discribe = discribe
+        self.date_end = date_end
+        self.status = status
+
+    def input_task(self):
+ 
